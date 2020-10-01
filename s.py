e6709b513456b504d3d498e533ef179d2446cec0
tony-122 <72181315+tony-122@users.noreply.github.com>
2020-10-01 16:13:19 +0530

    Update s.py
    change

--- s.py
+++ s.py
@@ -3,13 +3,13 @@ cap=turtle
 i=0
 j=1
 col=['red','white','red','blue','white']
-for x in range(100,10,-20):
+for z in range(100,10,-20):
     cap.begin_fill()
     cap.penup()
     cap.goto(0,20*j)
     j=j+1
     cap.pendown()
-    cap.circle(x)
+    cap.circle(z)
     cap.fillcolor(col[i])
     i=i+1
     cap.end_fill()
